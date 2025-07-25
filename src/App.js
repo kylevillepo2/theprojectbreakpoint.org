669b405a29f48b0e295e78724e94a851632685b7
kylevillepo2 <kvillepo@gmail.com>
2025-07-25 11:53:45 -0700

    should see analytics

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Events from "./pages/Events";
 import "./styles/App.css";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
