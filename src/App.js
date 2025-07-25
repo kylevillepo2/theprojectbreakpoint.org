18eb5d4f14efe5f43b9b306ed010c02e1ec933e3
kylevillepo2 <kvillepo@gmail.com>
2025-07-25 12:01:47 -0700

    NOW the analytics should work

--- src/App.js
+++ src/App.js
@@ -16,9 +16,9 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/events" element={<Events />} />
-          <Analytics />
         </Routes>
       </div>
+      <Analytics />
     </Router>
   );
 }
